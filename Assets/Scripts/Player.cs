cac441def56f3c2cde95e8ac8625887803fd60ae
Hugo Paiva <hugopaiva@outlook.com.br>
2024-10-13 15:53:23 -0300

    FEATURE: Inclusao do pulo FIX: Correção da corrida

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float Speed;
-    public float JumpForce;
-    public bool isJumping = false;
-
-
+    [Header("Controle do Movimento")] 
+    [SerializeField] public float SpeedWalk;
+    [SerializeField] public float SpeedRun;
+    [Header("Controle do Pulo")] 
+    [SerializeField] public float JumpForce;
+    [SerializeField] public bool isJumping = false;
     private Rigidbody2D rig;
     private Animator anim;
     void Awake() {
@@ -25,35 +26,70 @@ public class Player : MonoBehaviour
     void Update()
     {
         Move();
+        Jump();
     }
 
     void Move()
     {
         Vector3 movement = new Vector3(Input.GetAxis( "Horizontal"), 0f , 0f);
-        transform.position += movement * Time.deltaTime * Speed;
-        if(Input.GetAxis( "Horizontal") > 0f){
-            if(Input.GetKey(KeyCode.LeftShift))
-            {
-                anim.SetBool("run", true);
-            }
-            else
-            {
-                anim.SetBool("walk", true);
-            }
-            
-            transform.eulerAngles = new Vector3(0f,0f,0f);
-        }else if(Input.GetAxis( "Horizontal") < 0f){
-            if(Input.GetKey(KeyCode.LeftShift))
-            {
-                anim.SetBool("run", true);
-            }
-            else
-            {
-                anim.SetBool("walk", true);
-            }
+        if(!Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetBool("run", false);
+            transform.position += movement * Time.deltaTime * SpeedWalk;
         }else{
+            transform.position += movement * Time.deltaTime * SpeedRun;
+        }
+        if(Input.GetAxis( "Horizontal")!= 0f)
+        {
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                anim.SetBool("run", true);
+            }
+            else if(!Input.GetKey(KeyCode.LeftShift))
+            {
+                anim.SetBool("walk", true);
+            }
+        }
+        if(Input.GetAxis( "Horizontal") > 0f)
+        {
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }else if(Input.GetAxis( "Horizontal") < 0f)
+        {
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                anim.SetBool("run", true);
+            }
+            else if(!Input.GetKey(KeyCode.LeftShift))
+            {
+                anim.SetBool("walk", true);
+            }
+        }
+        else
+        {
             anim.SetBool("walk", false);
             anim.SetBool("run", false);
+        }
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+            anim.SetBool("jump", true);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.layer == 7){
+            isJumping = false;
+            anim.SetBool("jump", false);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        if(collision.gameObject.layer == 7){
+            isJumping = true;
         }
     }
 }
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class World : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Awake() {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
     }
 }
